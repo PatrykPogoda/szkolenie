bbed2f5f1c3bd393d36550f7ca0d1826cbfd4cda
PatrykPogoda <pogoda.patryk@gmail.com>
2015-04-20 06:14:21 +0200

    Notatki ze struktur i troche kodu.

--- struct/main.cpp
+++ struct/main.cpp
@@ -12,22 +12,41 @@ struct Dwa{
 };
 int main()
 {
-    Jeden obiektJ;
+    Jeden obiektJ, obiektJ2;
     Jeden *wsk = &obiektJ;
+    Jeden *wsk2 = &obiektJ2;
     Dwa obiektD;
 
     cout<<"Przed przypisaniem (randomowa wartosc): "<<obiektJ.x<<endl;
     cout<<"Przed przypisaniem (wskaznikiem wsk->): "<<wsk->x<<endl;
+    cout<<"Wartosc randomowa powinna sie zmieniac co odpalenie programu, ale nie w trakcie jego dzialania."<<
+    "Mozna to przesledzic po obiekcie 2."<<endl<<endl;
+    cout<<"Sprawdzam wskaznikiem co jest w obiecie drugim: "<<wsk2->x<<endl;
 
     obiektD.podajx(obiektJ, 10);
 
+    cout<<endl;
+
     cout<<"Pierwsze przypisanie: "<<obiektJ.x<<endl;
     cout<<"Pierwsze przypisanie (wskaznikiem wsk->): "<<wsk->x<<endl;
+    cout<<"Sprawdzam wskaznikiem co jest w obiecie drugim: "<<wsk2->x<<endl;
 
-    obiektD.podajx(obiektJ, 11);
+      obiektD.podajx(obiektJ, 11);
+
+    cout<<endl;
 
     cout<<"Drugie przypisanie: "<<obiektJ.x<<endl;
     cout<<"Drugie przypisanie (wskaznikiem wsk->): "<<wsk->x<<endl;
+    cout<<"Sprawdzam wskaznikiem co jest w obiecie drugim: "<<wsk2->x<<endl;
+
+
+    cout<<endl;
+    obiektJ.x = 12;
+    obiektJ2.x = 111;
+    cout<<"Zmieniam wartosc standadowo: "<<obiektJ.x<<endl;
+    cout<<"i przez wskaznik ja wypisuje: "<<wsk->x<<endl;
+    cout<<"i x w obiekcie 2 (po zmianie wartosci): "<<wsk2->x<<endl;
+
 
     return 0;
 }
